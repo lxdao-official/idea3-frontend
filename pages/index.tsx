aeb9d35c5a0b88283d46a884c1b3238942bb68bb
0x1998 <xx@xx.com>
2023-02-22 23:02:35 +0800

    Add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,7 +94,7 @@ export default function Home() {
             it reality."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
