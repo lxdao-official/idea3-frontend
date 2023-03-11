33ebf3079fbe57a2238e1c4d86cb0f69f7a93ae3
0x1998 <bit199801@gmail.com>
2023-03-11 10:54:01 +0800

    Fix text input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
 import { LXDAOLogo } from 'lxdao-ui';
 import {
@@ -25,8 +23,6 @@ import { List } from '../components/List';
 import MintDID from '../components/MintDID';
 import BindDID from '../components/BindDID';
 import { BindDidContext, DIDContext, useDID } from '../contexts/did.context';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const { showMintModalHandler } = useContext(DIDContext);
